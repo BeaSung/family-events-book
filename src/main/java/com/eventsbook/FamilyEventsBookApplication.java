098efcd52e6548c2bbecf59b7fcbc4c16b97a5ab
bea <eunbi1101@gmail.com>
2023-04-22 16:05:15 +0900

    first commit

---
+++ src/main/java/com/eventsbook/FamilyEventsBookApplication.java
@@ -0,0 +1,13 @@
+package com.eventsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyEventsBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyEventsBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventsbook/FamilyEventsBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyEventsBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
