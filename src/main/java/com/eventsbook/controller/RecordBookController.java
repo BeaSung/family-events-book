92063d18eb803ecce01308ae4355ee3078bab78f
bea <eunbi1101@gmail.com>
2023-04-23 23:55:40 +0900

    feat: RecordBook 목록 조회 API 스펙(Infinite scroll) 추가

--- src/main/java/com/eventsbook/controller/RecordBookController.java
+++ src/main/java/com/eventsbook/controller/RecordBookController.java
@@ -2,11 +2,14 @@ package com.eventsbook.controller;
 
 import com.eventsbook.controller.request.AddReceivedMoneyRecordRequest;
 import com.eventsbook.controller.request.AddSentMoneyRecordRequest;
+import com.eventsbook.controller.response.GetRecordBooksResponse;
 import com.eventsbook.service.RecordBookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -24,5 +27,10 @@ public class RecordBookController {
     @PostMapping("/received-money")
     public void addReceivedMoney(@RequestBody AddReceivedMoneyRecordRequest request) {
         service.addReceivedMoneyRecord(0L, request);
+    }
+
+    @GetMapping
+    public GetRecordBooksResponse getRecordBooks(@RequestParam(required = false) Long lastId) {
+        return new GetRecordBooksResponse();
     }
 }
---
+++ src/main/java/com/eventsbook/controller/response/GetRecordBooksResponse.java
@@ -0,0 +1,13 @@
+package com.eventsbook.controller.response;
+
+import java.util.List;
+
+public class GetRecordBooksResponse {
+
+    private List<GetRecordBookDTO> recordBooks;
+
+    public static class GetRecordBookDTO {
+        private Long id;
+        //
+    }
+}
